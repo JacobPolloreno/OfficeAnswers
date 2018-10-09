e7502dfbd10c678e864c847b7e482c107ca6b777
Jacob Polloreno <makaveliix16@gmail.com>
2018-10-09 02:55:42 -0700

    add documentation to build search index

--- src/officeanswers/search/build_search_index.py
+++ src/officeanswers/search/build_search_index.py
@@ -1,10 +1,26 @@
 import nmslib
 import numpy as np
 
-from ..preprocess import Hasher
-
 
 def build_search_index(embeddings: np.ndarray):
+    """
+    Build a search index for NMSLIB semantic search library
+
+    Args:
+        embeddings (numpy array) : array containing precomputed
+            embeddings that you want to search
+
+    Return:
+        a nmslib search index that can be cached for later or
+        used to perform searches
+
+
+    Usage:
+        >>> docs, embeds = build_document_embeddings(config)
+        >>> search = build_search_index(embeds)
+        >>> ...
+        >>> idxs, dists = search.knnQuery(dense_input, k=5)
+    """
     search_index = nmslib.init(method='hnsw', space='cosinesimil')
     search_index.addDataPointBatch(embeddings)
     search_index.createIndex({'post': 2}, print_progress=True)
